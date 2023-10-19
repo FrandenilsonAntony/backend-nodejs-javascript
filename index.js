ee73968f3b1717e284ea42ef8634a3663b59f89a
Frandenilson Antony <iabyantony88@gmail.com>
2023-10-19 18:28:05 +0000

    "Feat: Install `Thunder Client` and implemented `Express` into `index.js` with default endpoint"

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Aprimorando Backend");
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('Aperfeiçoando Backend with JavaScript, Nodejs, VScode, CodeSpace and Github')
+})
+
+app.listen(3000)
