6e98febc1f5bd47bfc4dbe3e4eba1ebde8649d55
Frandenilson Antony <iabyantony88@gmail.com>
2023-10-20 00:13:22 +0000

    "Feat: Create endpoint `/oi`"

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Aperfeiçoando Backend with JavaScript, Nodejs, VScode, CodeSpace and Github')
 })
 
-app.listen(3000)
+app.get('/oi', function (req, res) {
+  res.send('Cada gota salubre desperta o encanto pelo teu sorriso...')
+})
+
+app.listen(3000, function(){
+  console.log("App running on http://localhost:3000")
+})
