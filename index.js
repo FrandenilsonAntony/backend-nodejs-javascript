a6aaf0fcda80fd2e22576872f43236a592f9f637
Frandenilson Antony <iabyantony88@gmail.com>
2023-10-20 15:12:30 +0000

    "Feat: Implemented endpoint `Read By id` - `[GET] /items/:id`"

--- index.js
+++ index.js
@@ -18,10 +18,24 @@ app.get('/oi', function (req, res) {
 const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
 
 //READ ALL - [GET] /items
-
 app.get("/items", function (req, res){
    res.send(items)
 })
+
+//READ BY ID - [GET] /items/:id/
+app.get("/items/:id", function (req, res) {
+   //Acessamos o parâmetro de rota ID
+   //Subtraí 1 para corrigir a ordem de início da lista
+   const id = req.params.id -1
+   
+   //Acessmos o item da lista a partir do index.js
+   const item = items[id]
+  
+   //Exibe o item obtido
+  res.send(item)
+})
+
+
 
 // CREATE - [POST] /items
 app.post("/items", function (req, res){
