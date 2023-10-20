2f5f2335f6c6e7db162ea92aeb309e3b059b6f95
Frandenilson Antony <iabyantony88@gmail.com>
2023-10-20 15:52:09 +0000

    "Feat: Implemented endpoint `DELETE` - `[DELETE] /items/:id`"

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ app.get('/oi', function (req, res) {
 const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
 
 
-//READ ALL - [GET] /items
+// READ ALL - [GET] /items
 app.get("/items", function (req, res){
-   res.send(items)
+   res.send(items.filter(Boolean))
 })
 
 
-//READ BY ID - [GET] /items/:id/
+// READ BY ID - [GET] /items/:id/
 app.get("/items/:id", function (req, res) {
    //Acessamos o parâmetro de rota ID
    //Subtraí 1 para corrigir a ordem de início da lista
@@ -65,6 +65,21 @@ app.put("/items/:id", function (req, res){
   //Envia mensagem de sucesso!
   res.send(" Item Update by id successfully.")
 })
+
+
+// DELETE - [DELETE] /items/:id
+app.delete("/items/:id", function (req, res){
+  //Acessamos o parâmetro de rota ID
+   //Subtraí 1 para corrigir a ordem de início da lista
+  const id = req.params.id -1
+  
+  //Exclui a informação através do 'id'
+  delete items[id]
+
+  //Enviamos mensagem de sucesso
+  res.send("Item Deleted by id with successfully.")
+})
+
 
 app.listen(3000, function(){
   console.log("App running on http://localhost:3000")
