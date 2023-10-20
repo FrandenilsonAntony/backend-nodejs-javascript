e513e36e8a2f4434a19baa0a3b031c8818102847
Frandenilson Antony <iabyantony88@gmail.com>
2023-10-20 14:45:12 +0000

    "Feat: Implemented endpoint `Create` - `[POST] /items`"

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 
+//Sinaliza for the Express what the body of require will be ever in Json
+app.use(express.json())
+
 app.get('/', function (req, res) {
   res.send('Aperfeiçoando Backend with JavaScript, Nodejs, VScode, CodeSpace and Github')
 })
@@ -14,13 +17,23 @@ app.get('/oi', function (req, res) {
 
 const items = ["Java", "Android", "Kotlin", "Express", "NestJS"]
 
-//READ ALL-[GET] /itens
+//READ ALL - [GET] /items
 
 app.get("/items", function (req, res){
    res.send(items)
 })
 
+// CREATE - [POST] /items
+app.post("/items", function (req, res){
+  //Extrair informação do requerie of body
+  const item = req.body.name
 
+  //Insero ela na lista
+  items.push(item)
+
+  //Sended message of successfully
+  res.send("Items created with successfully.")
+})
 
 app.listen(3000, function(){
   console.log("App running on http://localhost:3000")
